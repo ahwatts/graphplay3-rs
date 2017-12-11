67daa075405d92abb51543355b994c95068af7dc
Andrew Watts <ahwatts@gmail.com>
2017-12-11 00:37:05 -0500

    Not sure where I was going with this, but this is an implementation.

--- src/gpgfx/mod.rs
+++ src/gpgfx/mod.rs
@@ -1,15 +1,22 @@
-use gfx::{VertexBuffer, Resources, Slice, IntoIndexBuffer};
+use gfx::{Resources, Slice, IntoIndexBuffer};
+use gfx::handle::Buffer;
 use gfx::traits::{FactoryExt, Pod};
 use gfx::pso::buffer::Structure;
 use gfx::format::Format;
 
 pub struct Geometry<V: Pod + Structure<Format>, R: Resources> {
-    vertices: VertexBuffer<V>,
+    vertices: Buffer<R, V>,
     indices: Slice<R>,
 }
 
 impl<V: Pod + Structure<Format>, R: Resources> Geometry<V, R> {
-    pub fn new<F: FactoryExt<R>, I: IntoIndexBuffer<R>>(factory: &mut F, vertices: &[V], elems: I) {
+    pub fn new<F, I>(factory: &mut F, vertices: &[V], elems: I) -> Geometry<V, R>
+        where F: FactoryExt<R>, I: IntoIndexBuffer<R>
+    {
         let (vbuf, elems) = factory.create_vertex_buffer_with_slice(vertices, elems);
+        Geometry {
+            vertices: vbuf,
+            indices: elems,
+        }
     }
 }
